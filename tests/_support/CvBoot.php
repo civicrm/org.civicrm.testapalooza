d803adaf7ae42d342a55057f437bbf396b88fdca
Tim Otten <totten@civicrm.org>
2016-02-03 13:23:25 -0700

    CvBoot - Change env handling
    In Ubuntu's php-cli, the php.ini overrides `variables_order` with `GPCS` --
    which destroys `$_ENV`.
    
    This seems pretty stupid...  perhaps they conflated `variables_order` with
    `request_order`.
    
    At any rate, we'll have to do statey-swappy `putenv()` things instead.

--- tests/_support/CvBoot.php
+++ tests/_support/CvBoot.php
@@ -18,8 +18,10 @@ function cv($cmd, $decode = 'json') {
 
 function _cv_run($cmd, $decode = 'json') {
   $descriptorSpec = array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => STDERR);
-  $env = $_ENV + array('CV_OUTPUT' => 'json');
-  $process = proc_open($cmd, $descriptorSpec, $pipes, __DIR__, $env);
+  $oldOutput = getenv('CV_OUTPUT');
+  putenv("CV_OUTPUT=json");
+  $process = proc_open($cmd, $descriptorSpec, $pipes, __DIR__);
+  putenv("CV_OUTPUT=$oldOutput");
   fclose($pipes[0]);
   $result = stream_get_contents($pipes[1]);
   fclose($pipes[1]);
